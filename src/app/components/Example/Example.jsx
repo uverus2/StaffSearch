a0295ad79fba78b33cae57e1ed8ebfb5d7580097
Nic <nic.neale@solent.ac.uk>
2021-01-29 16:34:54 +0000

    Update Example with Images and icons

--- src/app/components/Example/Example.jsx
+++ src/app/components/Example/Example.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Wrapper } from './Example.styled';
 
-class Example extends React.Component {
-    render() {
-        return (
-            <Wrapper>
-                Hello, mars.
-            </Wrapper>
-        );
-    }
-}
+import { Wrapper, HelloBox, Image } from './Example.styled';
+import PhoneSVG from 'Public/icons/phone.svg';
+
+import worldImageURL from 'Public/images/world.png';
+
+const Example = () => (
+    <Wrapper>
+        <HelloBox>
+            Hello world :) Want to <PhoneSVG />?
+        </HelloBox>
+        <HelloBox $align="right">
+            Hello human :) Can do!
+            <Image src={worldImageURL} alt="World" />
+        </HelloBox>
+    </Wrapper>
+);
 
 export default Example;
--- src/app/components/Example/Example.styled.jsx
+++ src/app/components/Example/Example.styled.jsx
@@ -3,5 +3,16 @@ import styled from 'styled-components';
 // https://styled-components.com/
 
 export const Wrapper = styled.div`
-    background:red;
+    background: #dbe3ff;
+    padding: 5rem;
+`;
+
+export const HelloBox = styled.div`
+    font-size: 2rem;
+    text-align: ${({ $align }) => $align ? $align : 'left'};
+`;
+
+export const Image = styled.img`
+    width: 7rem;
+    height: 7rem;
 `;
