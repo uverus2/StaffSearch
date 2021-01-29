c2379e8d1741b87d4d05220be747b082530c34d8
Nic <nic.neale@solent.ac.uk>
2021-01-29 16:35:52 +0000

    Use functional component

--- src/core/routing/Router.jsx
+++ src/core/routing/Router.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import routes from './routes';
 
-class Router extends React.Component {
-    render() {
-        return (
-            <Switch>
-                {routes.map((route, index) => (
-                    <Route key={index} {...route} />
-                ))}
-            </Switch>
-        );
-    }
-}
+const Router = () => (
+    <Switch>
+        {routes.map((route, index) => (
+            <Route key={index} {...route} />
+        ))}
+    </Switch>
+)
 
 export default Router;
