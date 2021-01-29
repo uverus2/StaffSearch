06e1870c1161699f7d3334cf962fc5b01653f736
Nic <nic.neale@solent.ac.uk>
2021-01-29 16:34:43 +0000

    Add global style

---
+++ src/app/global/Global.styled.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export default createGlobalStyle`
+    body {
+        padding: 0;
+        margin: 0;
+    }
+
+    body, input, button, textarea {
+        font-size: 1rem;
+        font-family: Calibri, 'Trebuchet MS', sans-serif;
+    }
+`;
