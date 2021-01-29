8dc1075f4bd75fbe1a5439500065fab5e8d6564f
Nic <nic.neale@solent.ac.uk>
2021-01-29 16:36:22 +0000

    Add global and public aliases

--- webpack/aliases.js
+++ webpack/aliases.js
@@ -4,7 +4,9 @@ module.exports = {
     App: path.resolve(__dirname, '../src/app/'),
     Components: path.resolve(__dirname, '../src/app/components/'),
     Core: path.resolve(__dirname, '../src/core/'),
+    Global: path.resolve(__dirname, '../src/app/global/'),
     Pages: path.resolve(__dirname, '../src/app/pages/'),
+    Public: path.resolve(__dirname, '../public/'),
     Redux: path.resolve(__dirname, '../src/core/redux/'),
     Routing: path.resolve(__dirname, '../src/core/routing/'),
     Util: path.resolve(__dirname, '../src/core/util')
